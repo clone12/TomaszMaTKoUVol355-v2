dd1ee9b37017f3b61d069afa750aaab6dec897ad
clone12 <clones007@gmail.com>
2022-11-21 17:43:45 +0100

    Zadanie 5
    Problem z tym systemem Input

--- Assets/Scripts/UI/UIMenuController.cs
+++ Assets/Scripts/UI/UIMenuController.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 namespace TKOU.SimAI.UI
 {
@@ -26,12 +27,29 @@ namespace TKOU.SimAI.UI
         [Tooltip("Set to TRUE to automatically start the game on play. Useful for testing.")]
         private bool autostart = false;
 
+        [SerializeField] MyButton[] buttonList;
+        public int selectedButton = 0;
+
+        public delegate void ButtonAction();
+
         #endregion Variables
 
         #region Unity methods
 
         private void Awake()
         {
+            buttonList = new MyButton[2];
+
+            buttonList[0].image = GameObject.Find("UI-Button-Play").GetComponent<Image>();
+            buttonList[0].image.color = Color.yellow;
+            buttonList[0].action = PlayButton_OnClick;
+
+            buttonList[1].image = GameObject.Find("UI-Button-Quit").GetComponent<Image>();
+            buttonList[1].image.color = Color.white;
+            buttonList[1].action = QuitButton_OnClick;
+
+            //Problem z tymi Inputami nie rozumiem do konca tego
+
             playButton.onClick.AddListener(PlayButton_OnClick);
             quitButton.onClick.AddListener(QuitButton_OnClick);
 
@@ -47,9 +65,49 @@ namespace TKOU.SimAI.UI
             }
         }
 
+        private void Update()
+        {
+            if (UnityEngine.Input.GetAxis("Vertical") > 0)
+            {
+                MoveToNextButton();
+            }
+            else if (UnityEngine.Input.GetAxis("Vertical") < 0)
+            {
+                MoveToPreviousButton();
+            }
+            
+            if (UnityEngine.Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                buttonList[selectedButton].action();
+            }
+        }
+
         #endregion Unity methods
 
         #region Private methods
+
+        void MoveToNextButton()
+        {
+            buttonList[selectedButton].image.color = Color.white;
+            selectedButton++;
+            if (selectedButton >= buttonList.Length)
+            {
+                selectedButton = 0;
+            }
+
+            buttonList[selectedButton].image.color = Color.yellow;
+        }
+
+        void MoveToPreviousButton()
+        {
+            buttonList[selectedButton].image.color = Color.white;
+            selectedButton--;
+            if (selectedButton < 0)
+            {
+                selectedButton = (buttonList.Length - 1);
+            }
+            buttonList[selectedButton].image.color = Color.yellow;
+        }
 
         #region Event callbacks
 
@@ -80,6 +138,14 @@ namespace TKOU.SimAI.UI
             mainMenuObject.SetActive(!gameController.IsGameRunning);
         }
 
+
         #endregion Private methods
+
+        [System.Serializable]
+        public struct MyButton
+        {
+            public Image image;
+            public ButtonAction action;
+        }
     }
 }
